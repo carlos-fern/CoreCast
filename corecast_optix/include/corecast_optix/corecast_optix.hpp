c6b0b53254ddd863612f77ac0384114ece348ee5
Carlos Fernandez-Martinez <carlosfernandez1989@gmail.com>
2026-02-22 10:10:00 -0800

    Central import point

---
+++ corecast_optix/include/corecast_optix/corecast_optix.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "corecast_optix/corecast_optix_context.hpp"
+#include "corecast_optix/corecast_optix_module.hpp"
+#include "corecast_optix/corecast_optix_program_registry.hpp"
+#include "corecast_optix/corecast_optix_pipeline.hpp"
+#include "corecast_optix/corecast_optix_launch.hpp"
+#include "corecast_optix/corecast_optix_sbt.hpp"
