0b693f27075554ad58d5b9f3c7dd00781bdb143f
carlos <carlosfernandez1989@gmail.com>
2026-04-05 12:55:12 -0700

    save point

--- corecast_optix/include/corecast_optix/corecast_optix.hpp
+++ corecast_optix/include/corecast_optix/corecast_optix.hpp
@@ -8,15 +8,24 @@ class CoreCastOptix : public std::enable_shared_from_this<CoreCastOptix> {
 
     CoreCastOptix(auto settings);
 
-    std::shared_ptr<CoreCastWorkflow> create_coresac_pipeline(){
+    std::shared_ptr<CoreCastWorkflow> create_coresac_pipeline(std::string &workflow_name, workflow_options &workflow_options){
 
-        return std::make_shared<CoreCastWorkflow>(shared_from_this(), workflow_options);
-        
+        auto workflow = make_shared<WorkflowType>(shared_from_this(), workflow_options);
+        workflows_[workflow_name] = workflow;
+        return workflow;
+    }
+
+    std::shared_ptr<CoreCastWorkflow> get_workflow(std::string &workflow_name){
+        return workflows_[workflow_name];
+    }
+
+    void destroy_workflow(std::string &workflow_name){
+        workflows_.erase(workflow_name);
     }
 
 
     private:
-    CoreCastOptixContext context_;
+    unordered_map<std::string, WorkflowType> workflows_;
 
 
     template <typename WorkflowType>
